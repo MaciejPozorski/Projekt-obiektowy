b3bf26b99b0260d34416643c28da001c3b35c439
Przemek <przemolsz29@gmail.com>
2021-04-06 17:54:32 +0200

    blabla

--- Ostatni_świecie/Program.cs
+++ Ostatni_świecie/Program.cs
@@ -1,4 +1,4 @@
- ﻿using System;
+﻿using System;
 
 namespace Ostatni_świecie
 {
@@ -6,7 +6,47 @@ namespace Ostatni_świecie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" zmiana z vs macidsfdsfej");
+            int start = 0;
+
+            while (start == 0)
+            {
+                Console.WriteLine("|==============================|");
+                Console.WriteLine("|         1. Nowa gra          |");
+                Console.WriteLine("|        2. Wczytaj grę        |");
+                Console.WriteLine("|    3. Zasady i informacje    |");
+                Console.WriteLine("|          4. Autorzy          |");
+                Console.WriteLine("|           5. Wyjdź           |");
+                Console.WriteLine("|==============================|");
+
+                Console.WriteLine("Wybierz opcję: ");
+
+                switch (int.Parse(Console.ReadLine()))
+                {
+                    case 1:
+                        Console.Clear();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.WriteLine("|==============================|");
+                        Console.WriteLine("|       Maciej Pozorski        |");
+                        Console.WriteLine("|     Przemysław Szymański     |");
+                        Console.WriteLine("|==============================|");
+                        Console.WriteLine("Wciśnij dowolny przycisk, aby wrócić do menu.");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 5:
+                        start++;
+                        break;
+                }
+            }
         }
     }
 }
+
