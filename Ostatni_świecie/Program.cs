f3e3eab68e65ae10c9d278207083eabdaef4e3d6
Maciej Pozorski <mpozorski01@gmail.com>
2021-04-06 17:19:48 +0200

    usunięcie "maciej"

--- Ostatni_świecie/Program.cs
+++ Ostatni_świecie/Program.cs
@@ -6,7 +6,7 @@ namespace Ostatni_świecie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" !!! z vs maciej");
+            Console.WriteLine(" !!! z vs");
         }
     }
 }
