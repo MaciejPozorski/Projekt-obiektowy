22edf25361399d4c156db6892050bd4c9aadc648
Maciej Pozorski <mpozorski01@gmail.com>
2021-04-06 17:47:13 +0200

    zmiany

---
+++ Ostatni_świecie/Program.cs
@@ -0,0 +1,12 @@
+ ﻿using System;
+
+namespace Ostatni_świecie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" zmiana z vs maciej");
+        }
+    }
+}
