f95ae36ea89907e647a1c98b72b8593e03a79f5b
Przemek <przemolsz29@gmail.com>
2021-04-06 18:34:45 +0200

    V1.1

--- Ostatni_świecie/Menu.cs
+++ Ostatni_świecie/Menu.cs
@@ -4,7 +4,7 @@ namespace Ostatni_świecie
 {
     class Menu
     {
-        public void MenuGlowne()
+        public void MainMenu()
         {
             int start = 0;
 
@@ -21,26 +21,44 @@ namespace Ostatni_świecie
 
                 Console.WriteLine("Wybierz opcję: ");
 
-                switch (int.Parse(Console.ReadLine()))
+                try
                 {
-                    case 1:
-                        Console.Clear();
-                        break;
-                    case 2:
-                        Console.Clear();
-                        break;
-                    case 3:
-                        Console.Clear();
-                        break;
-                    case 4:
-                        Console.Clear();
-                        break;
-                    case 5:
-                        Console.Clear();
-                        break;
-                    case 6:
-                        start++;
-                        break;
+                    int option = int.Parse(Console.ReadLine());
+
+                    switch (option)
+                    {
+                        case 1:
+                            Console.Clear();
+                            break;
+                        case 2:
+                            Console.Clear();
+                            break;
+                        case 3:
+                            Console.Clear();
+                            break;
+                        case 4:
+                            Console.Clear();
+                            break;
+                        case 5:
+                            Console.Clear();
+                            break;
+                        case 6:
+                            start++;
+                            break;
+                        default:
+                            Console.Clear();
+                            Console.WriteLine("Nie ma takiej opcji! Wciśnij dowolny przycisk i spróbuj ponownie.");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+                    }
+                }
+                catch (System.FormatException)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Nieznane polecenie! Wciśnij dowolny przycisk i spróbuj ponownie.");
+                    Console.ReadKey();
+                    Console.Clear();
                 }
             }
         }
--- Ostatni_świecie/Program.cs
+++ Ostatni_świecie/Program.cs
@@ -22,39 +22,57 @@ namespace Ostatni_świecie
                 
                 Console.WriteLine("Wybierz opcję: ");
 
-                switch (int.Parse(Console.ReadLine()))
+                try
                 {
-                    case 1:
-                        Console.Clear();
-                        game.MenuGlowne();
-                        Console.Clear();
-                        break;
-                    case 2:
-                        Console.Clear();
-                        break;
-                    case 3:
-                        Console.Clear();
-                        Console.WriteLine("|==============================|");
-                        Console.WriteLine("|   Tu kiedyś będą zasady ;)   |");
-                        Console.WriteLine("|                              |");
-                        Console.WriteLine("|==============================|");
-                        Console.WriteLine("Wciśnij dowolny przycisk, aby wrócić do menu.");
-                        Console.ReadKey();
-                        Console.Clear();
-                        break;
-                    case 4:
-                        Console.Clear();
-                        Console.WriteLine("|==============================|");
-                        Console.WriteLine("|       Maciej Pozorski        |");
-                        Console.WriteLine("|     Przemysław Szymański     |");
-                        Console.WriteLine("|==============================|");
-                        Console.WriteLine("Wciśnij dowolny przycisk, aby wrócić do menu.");
-                        Console.ReadKey();
-                        Console.Clear();
-                        break;
-                    case 5:
-                        start++;
-                        break;
+                    int option = int.Parse(Console.ReadLine());
+
+                    switch (option)
+                    {
+                        case 1:
+                            Console.Clear();
+                            game.MainMenu();
+                            Console.Clear();
+                            break;
+                        case 2:
+                            Console.Clear();
+                            break;
+                        case 3:
+                            Console.Clear();
+                            Console.WriteLine("|==============================|");
+                            Console.WriteLine("|   Tu kiedyś będą zasady ;)   |");
+                            Console.WriteLine("|                              |");
+                            Console.WriteLine("|==============================|");
+                            Console.WriteLine("Wciśnij dowolny przycisk, aby wrócić do menu.");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+                        case 4:
+                            Console.Clear();
+                            Console.WriteLine("|==============================|");
+                            Console.WriteLine("|       Maciej Pozorski        |");
+                            Console.WriteLine("|     Przemysław Szymański     |");
+                            Console.WriteLine("|==============================|");
+                            Console.WriteLine("Wciśnij dowolny przycisk, aby wrócić do menu.");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+                        case 5:
+                            start++;
+                            break;
+                        default:
+                            Console.Clear();
+                            Console.WriteLine("Nie ma takiej opcji! Wciśnij dowolny przycisk i spróbuj ponownie.");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+                    }
+                }
+                catch (System.FormatException)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Nieznane polecenie! Wciśnij dowolny przycisk i spróbuj ponownie.");
+                    Console.ReadKey();
+                    Console.Clear();
                 }
             }
         }
