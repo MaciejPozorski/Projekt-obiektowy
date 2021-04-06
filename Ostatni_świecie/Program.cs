b933c0caf70d1233114990b1aa6c090b5ebff166
Przemek <przemolsz29@gmail.com>
2021-04-06 17:04:09 +0200

    Test

--- Ostatni_świecie/Program.cs
+++ Ostatni_świecie/Program.cs
@@ -6,7 +6,7 @@ namespace Ostatni_świecie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("!!!!!");
         }
     }
 }
