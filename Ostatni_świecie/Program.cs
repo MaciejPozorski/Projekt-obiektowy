51279a401d4927e0408f6e9eb84f6c74572b13f5
Maciej Pozorski <mpozorski01@gmail.com>
2021-04-06 17:12:27 +0200

    Zmaina hello wordl

--- Ostatni_świecie/Program.cs
+++ Ostatni_świecie/Program.cs
@@ -6,7 +6,7 @@ namespace Ostatni_świecie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Test z git huba");
+            Console.WriteLine(" !!! z vs maciej");
         }
     }
 }
